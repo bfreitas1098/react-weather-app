bb2e5c466909151890d1dbc106aff4f688f22e77
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-02 12:07:19 -0500

    Styled the app and utilized bootstrap grids in the JSX

--- src/App.js
+++ src/App.js
@@ -7,23 +7,40 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h4>Boca Raton, USA</h4>
-        <span>Now</span>
-        <br />
-        <img src="https://cdn.onlinewebfonts.com/svg/img_154.png" />
+        <h4 className="mt-2">Boca Raton, USA</h4>
+        <div className="date">Wednesday 11:00 AM</div>
+        <img
+          src="https://cdn.onlinewebfonts.com/svg/img_154.png"
+          alt="Weather icon"
+        />
         <h1>50°</h1>
-        <span>
-          <strong>C</strong>
-        </span>
-        <span>
-          <strong>F</strong>
-        </span>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <a href="/" rel="noreferrer" className="celsius-link">
+              C
+            </a>
+          </div>
+          <div className="col-sm unit-divider">
+            <strong>|</strong>
+          </div>
+          <div className="col-sm">
+            <a href="/" rel="noreferrer" className="fahrenheit-link">
+              F
+            </a>
+          </div>
+        </div>
         <br />
-        <h5>Partly Cloudy</h5>
-        <h6>Wind</h6>
-        <div>5 mph</div>
-        <h6>Humidity</h6>
-        <div>60%</div>
+        <h5 className="mt-3">Partly Cloudy</h5>
+        <div className="row">
+          <div className="col-sm">
+            <h6 className="mt-5">Wind</h6>
+            <div className="wind-conditions">5 mph</div>
+          </div>
+          <div className="col-sm">
+            <h6 className="mt-5">Humidity</h6>
+            <div className="humidity-conditions">60%</div>
+          </div>
+        </div>
       </div>
     </div>
   );
