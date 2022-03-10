956560bbf1ac2de076c1d3f9816e6539920de03d
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-10 18:05:47 -0500

    Moved the variables that hold the api call into its own function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,14 @@ function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    const apiKey = "dcc17c3d57640b0236dc66644f1c5237";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -30,11 +38,7 @@ function WeatherForecast(props) {
       </div>
     );
   } else {
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    const apiKey = "dcc17c3d57640b0236dc66644f1c5237";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayForecast);
+    load();
 
     return null;
   }
